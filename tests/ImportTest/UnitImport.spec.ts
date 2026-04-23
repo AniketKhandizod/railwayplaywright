6a16430864f5844145db758ffce5072ad704a8d6
aniket-selldo <aniket.khandizod@sell.do>
2026-04-23 17:06:59 +0530

    initial commit

--- tests/ImportTest/UnitImport.spec.ts
+++ tests/ImportTest/UnitImport.spec.ts
@@ -2,5 +2,13 @@ import { test } from "@playwright/test";
 
 test("Import Unit - Unit Import", async ({ page }) => {
   await page.goto("https://selldo.com/");
-  await page.waitForTimeout(10000);
+
+  const adminEmail = process.env.ADMIN_EMAIL;
+  const password = process.env.PASSWORD;
+  const fullAccessApi = process.env.FULL_ACCESS_API;
+  const clientId = process.env.CLIENT_ID;
+  const restrictedApi = process.env.RESTRICTED_API;
+
+  console.log(adminEmail, password, fullAccessApi, clientId, restrictedApi);
+
 });
